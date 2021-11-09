93dc8df4a40724ca1d21772c9f2dd80cf8988432
TKreateDev <tanbir98@hotmail.it>
2021-11-09 16:01:34 +0000

    hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>hello</p>
       <form action="http://localhost:8000/pdf" method="POST">
         <input type='text' name='name' placeholder='Your name' />
         <textarea placeholder='Tell me about yourself' name='about'> </textarea>
