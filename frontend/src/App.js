098072210ab177e2a73063db5e5b201f44c9d9a2
Gil <gilbertjvirgo@gmail.com>
2021-11-09 16:21:57 +0000

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <p>hello world</p>
-      <form action="http://localhost:8000/pdf" method="POST">
-        <input type='text' name='name' placeholder='Your name' />
-        <textarea placeholder='Tell me about yourself' name='about'> </textarea>
-        <input type='submit' value='Create PDF' />
-      </form>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>hello test</p>
+			<form action="http://localhost:8000/pdf" method="POST">
+				<input type="text" name="name" placeholder="Your name" />
+				<textarea placeholder="Tell me about yourself" name="about">
+					{" "}
+				</textarea>
+				<input type="submit" value="Create PDF" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
