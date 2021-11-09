15b886c0905fbf145bea7b37575fc731eb2a1a0f
TKreateDev <tanbir98@hotmail.it>
2021-11-09 16:04:05 +0000

    npm i add

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,15 @@ git pull
 
 echo "Building frontend..."
 cd frontend
+npm i
 npm run build
 
 echo "Running the backend (in the background)..."
 cd ../backend
+npm i
 nohup node . &>./log &
 
 echo "Running webhook (in the background)..."
 cd ..
+npm i
 nohup node ./webhook &>./log &
