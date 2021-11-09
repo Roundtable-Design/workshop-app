7bde5ce23bb405fe748183d9fd7c0e5fb4198732
Gil <gilbertjvirgo@gmail.com>
2021-11-09 16:20:41 +0000

    Added * route

--- webhook.js
+++ webhook.js
@@ -5,18 +5,22 @@ const http = require("http");
 dotenv.config();
 
 var handler = createHandler({
-    path: "/webhook",
-    secret: process.env.GIT_SECRET, // Use the .env file
+	path: "/webhook",
+	secret: process.env.GIT_SECRET, // Use the .env file
 });
 
 http.createServer((req, res) => {
-    handler(req, res, () => res.end("Not found"));
+	handler(req, res, () => res.end("Not found"));
 }).listen(7777, () => console.log("Starting webhook handler..."));
+
+handler.on("*", (data) => {
+	console.log("FROM *", data);
+});
 
 handler.on("error", console.error);
 
 handler.on("push", () => {
-    console.log("Recieved a push event!");
+	console.log("Recieved a push event!");
 
-    exec(`bash ./deploy.sh >> ./local`);
+	exec(`bash ./deploy.sh >> ./local`);
 });
