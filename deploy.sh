643df40b90f1c42b9496680f59e2c1e5cc156815
Gil <gilbertjvirgo@gmail.com>
2021-11-11 14:14:03 +0000

    Changes to deploy sh

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,19 @@
 echo "Killing currently running processes..."
 killall node
 
-echo "Using --lts version of node"
-nvm install --lts
-
 echo "Pulling latest code..."
 git pull
 
 echo "Building frontend..."
 cd frontend
-nohup npm i; npm run build &>./log &
+nohup npm i; npm run build &>>./log &
 
 
 echo "Running the backend (in the background)..."
 cd ../backend
-nohup npm i; node . &>./log &
+nohup npm i; node . &>>./log &
 
 
 echo "Running webhook (in the background)..."
 cd ..
-nohup npm i; node ./webhook &>./log &
+nohup npm i; node ./webhook &>>./log &
